ede5d7ff098bb786dff7690ea50b4fa0a10ee82f
vnctwong <vincentkywong@gmail.com>
2019-02-08 01:29:35 +0000

    finished anon-functions.js

---
+++ anon-functions.js
@@ -0,0 +1,34 @@
+// /*Complete the following code for the assertion to pass. The
+// result should be an array of numbers corresponding to the x-y
+// pairs provided in the input array (ie: calculate z given x
+// and y).*/
+// // x^2 + y^2 = z^2
+
+var input = [
+  { x: 3, y: 4 },
+  { x: 12, y: 5 },
+  { x: 8, y: 15 }
+];
+var result = input.map(input => (Math.pow(input.x, 2) + Math.pow(input.y, 2)));
+
+
+console.log(result[0]);
+
+console.log(result[0]);
+console.log(result[1]);
+console.log(result[2]);
+
+// var input = [
+//   { x: 3,
+//     y: 4 },
+//   { x: 12,
+//     y: 5 },
+//   { x: 8,
+//     y: 15 }
+//   ];
+
+//   input[0].['x']
+
+
+  // console.log( input['x'] )
+
